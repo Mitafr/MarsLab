e11f6854a07cc5ce892ff96ceb0e35981573b0fc
Martin Pelletier <mpelle1@a-139160.ups.u-psud.fr>
2019-12-04 16:30:34 +0100

    todo: bloc

--- main.m
+++ main.m
@@ -2,13 +2,53 @@ clear;
 clc;
 
 Yini = single(imread('test2.jpg'));
-#Yini = single(imread('Mars_dunes.jpg'));
+%Yini = single(imread('Mars_dunes2.jpg'));
+
+ltot = size(Yini,1);
+ctot = size(Yini,2);
+trois = size(Yini,3);
+X = reshape(Yini, [ltot*ctot,3]);
+
+n=size(X,1);
+nl = 5;
+l = floor(ltot/nl);
+nc = 5;
+c = floor(ctot/nc);
+excedingLine = mod(ltot,nl);
+excedingCol = mod(ctot,nc);
+tbloc=[];
+counter=0;
+for i = 1:nl*nc:l*c
+    xi=(i-1)*nl*nc;
+    bloc=X(xi+1:(xi+1+nl*nc)-1,:);
+    %{
+    if ltot-xi < 0
+        kk=xi+1-nl*nc;
+        kkk=ltot;
+        bloc=X(kk:kkk,:);
+        oqz1=5;
+    end
+    %}
+    moyenneBloc=mean(bloc);
+    stdBloc=std(bloc);
+    
+    [P,E,Ip] = codeur_ACP(bloc,3);
+    Xfinal = decodeur_ACP(P, E);
+    
+    tbloc=cat(1,tbloc, Xfinal .* repmat(stdBloc,[size(bloc,1) 1]) + repmat(moyenneBloc,[size(bloc,1) 1]));
+    counter=counter+1;
+end
+finalData = reshape(tbloc, ltot,ctot,trois);
+compare_images(Yini, finalData);
+imwrite(uint8(Yini), "output_ini.jpg");
+imwrite(uint8(finalData), "output_final.jpg");
+
 
 function show_image(image_input, title)
   image(uint8(image_input))
   title(title)
   axis equal
-endfunction
+end
 
 function compare_images(I1, I2)
   figure,
@@ -17,60 +57,31 @@ function compare_images(I1, I2)
   figure,
   set(gcf,'numbertitle','off','name','image finale'),
   imshow(uint8(I2));
-endfunction
+end
 function X = decodeur_ACP(P,E)
   X = P*transpose(E);
   return;
-endfunction
-ltot = size(Yini,1);
-ctot = size(Yini,2);
-trois = size(Yini,3);
-X = reshape(Yini, [ltot*ctot,3]);
-
-n=size(X,1);
-nl = 50;
-l = floor(ltot/nl);
-nc = 50;
-c = floor(ctot/nc);
-excedingLine = mod(ltot,nl);
-excedingCol = mod(ctot,nc);
-tbloc=[];
+end
 
 function [P,E,Ip] = codeur_ACP(X,p)  
   moyenneBloc=mean(X);
   stdBloc=std(X);
   Xcentre = X - repmat(moyenneBloc, size(X,1), 1);
-  Xreduite = (e*stdBloc);
+  Xreduite = (eye()*stdBloc);
   Xstandard = (X - repmat(moyenneBloc,[size(X,1) 1])) ./ repmat(stdBloc,[size(X,1) 1]);
-  # Supression des NaN
+  % Supression des NaN
   Xstandard(isnan(Xstandard))=0;
 
   Vs = (Xcentre'*Xcentre)/size(Xcentre,2)-1;
   [E,D] = eig(cov(Xstandard));
+  Ip=cumsum(E) / sum(E); %temporaire
   
-  # Vs = E*((D/size(X,1)).^(1/2));
+  % Vs = E*((D/size(X,1)).^(1/2));
   p=size(X,2)-p;
   for i = 1:p
     E(:,i) =[];
   end
   P=Xstandard*E;
-  Ip=cumsum(E) / sum(E); #temporaire
   return;
-endfunction
-
-for i = 1:l*c
-    xi=(i-1)*nl*nc;
-    bloc=X(xi+1:(xi+1+nl*nc)-1,:);
-    moyenneBloc=mean(bloc);
-    stdBloc=std(bloc);
-    
-    [P,E,Ip] = codeur_ACP(bloc,1);
-    Xfinal = decodeur_ACP(P, E);
-    
-    tbloc=cat(1,tbloc, Xfinal .* repmat(stdBloc,[size(bloc,1) 1]) + repmat(moyenneBloc,[size(bloc,1) 1]));
-endfor
-finalData = reshape(tbloc, [ltot,ctot,3]);
-compare_images(Yini, finalData);
-imwrite(uint8(Yini), "output_ini.jpg");
-imwrite(uint8(finalData), "output_final.jpg");
+end
 
